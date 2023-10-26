c460b6e1320ee861097d04c96e55d42a562b5de1
gregelious <90730434+gregelious@users.noreply.github.com>
2023-10-26 01:44:25 -0400

    made train bigger

--- Crossy Road/Assets/Scripts/TrainMovement.cs
+++ Crossy Road/Assets/Scripts/TrainMovement.cs
@@ -6,23 +6,23 @@ public class TrainMovement : MonoBehaviour
 {
     public float xSpeed = 0; //horizontal speed
 
-    private float xBorder = 400f; // left & right border
+    private float xBorder = 400f; // right border
 
 
     // Start is called before the first frame update
     void Start()
     {
-        xSpeed = 0.0125f; // moves to the right
+        xSpeed = 0.0125f; // sets speed
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x + xSpeed*8, transform.position.y); // goes right
+        transform.position = new Vector2(transform.position.x + xSpeed*8, transform.position.y); // goes right at 8 times the speed
 
         if (transform.position.x >= xBorder) // if past the right border
         {
-            transform.position = new Vector2(-80, transform.position.y); // goes left
+            transform.position = new Vector2(-80, transform.position.y); // teleport to left of map
 
         }
     }
