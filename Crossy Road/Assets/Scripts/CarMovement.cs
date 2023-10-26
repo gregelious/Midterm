756899aa25e7925f625abdc094d68a3277b3024a
gregelious <90730434+gregelious@users.noreply.github.com>
2023-10-25 23:40:06 -0400

    Update CarMovement.cs

--- Crossy Road/Assets/Scripts/CarMovement.cs
+++ Crossy Road/Assets/Scripts/CarMovement.cs
@@ -8,6 +8,7 @@ public class CarMovement : MonoBehaviour
 
     private float xBorder = 10f; // left & right border
 
+    private bool xMove = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,22 +16,27 @@ public class CarMovement : MonoBehaviour
         if (transform.position.y < 4)
         {
             xSpeed = 0.0125f * 250 / 100;
+            xMove = true;
         }
         if (transform.position.y < 2)
         {
             xSpeed = 0.0125f * 200 / 100;
+            xMove = false;
         }
         if (transform.position.y < 0)
         {
             xSpeed = 0.0125f * 150 / 100;
+            xMove = true;
         }
         if (transform.position.y < -1)
         {
             xSpeed = 0.0125f * 125 / 100;
+            xMove = false;
         }
         if (transform.position.y < -3)
         {
             xSpeed = 0.0125f;
+            xMove = true;
         }
     }
 
@@ -38,12 +44,26 @@ public class CarMovement : MonoBehaviour
     void Update()
     {
 
-        transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y); // goes right
 
-        if (transform.position.x >= xBorder) // if past the right border
+        if (xMove == true)
         {
-            transform.position = new Vector2(-xBorder, transform.position.y); // goes left
+            transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y); // goes right
 
+            if (transform.position.x >= xBorder) // if past the right border
+            {
+                transform.position = new Vector2(-xBorder, transform.position.y); // goes left
+
+            }
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y); // goes right
+
+            if (transform.position.x <= -xBorder) // if past the right border
+            {
+                transform.position = new Vector2(xBorder, transform.position.y); // goes left
+
+            }
         }
     }
 }
